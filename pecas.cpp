4697b9bc5c033dd7ae9a257960e9b4cea451787f
Gabryel Batista <78879283+gabryelfbatista@users.noreply.github.com>
2021-08-19 23:56:25 -0300

    Add files via upload

--- pecas.cpp
+++ pecas.cpp
@@ -1,3 +1,43 @@
 #include "pecas.h"
 #include <iostream>
+#include <vector>
+#include <string>
+
+Pecas::Pecas(){};
+Pecas::~Pecas(){};
+
+void Pecas::imprime_tabuleiro(){
+    for (int i=0; i<7; i++){
+        std::cout << A[i] << "   ";
+    }
+    std::cout << std::endl;
+    for (int i=0; i<7; i++){
+        std::cout << B[i] << "   ";
+    }
+    std::cout << std::endl;
+    for (int i=0; i<7; i++){
+        std::cout << C[i] << "   ";
+    }
+    std::cout << std::endl;
+    for (int i=0; i<7; i++){
+        std::cout << D[i] << "   ";
+    }
+    std::cout << std::endl;
+    for (int i=0; i<7; i++){
+        std::cout << E[i] << "   ";
+    }
+    std::cout << std::endl;
+    for (int i=0; i<7; i++){
+        std::cout << F[i] << "   ";
+    }
+    std::cout << std::endl;
+    for (int i=0; i<7; i++){
+        std::cout << G[i] << "   ";
+    }
+    std::cout << std::endl;
+    for (int i=0; i<7; i++){
+        std::cout << H[i] << "   ";
+    }
+    
+};
 
--- xadrez.cpp
+++ xadrez.cpp
@@ -1,8 +1,73 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include "pecas.h"
 
 int main() {
+    std::ifstream arq;
+    std::string linha;
+    std::vector<std::string> linhas;
+    arq.open("jogo01.pgn");
+    
+    while(arq.eof()==false){
+        std::getline(arq, linha);
+        linhas.push_back(linha);
+    };
+
+    std::vector<std::string> jogadas;
+
+    auto it = jogadas.begin();
+    jogadas.insert(it, linhas[11]);
+
+    std::cout << jogadas[0] << std::endl;
+
+    linha = jogadas[0];
+
+    std::vector<std::string> jogada_por_jogada;
+
+    int tamanho = linha.size();
+    std::cout << tamanho << std::endl;
+    std::size_t vazio;
+    std::string str3;
+    std::size_t tamanho_ultimo = linha.find_last_of(" ");
+    std::string ultimo = linha.substr(tamanho_ultimo+1);
+    for (int i=0; i<tamanho; i++){
+        vazio = linha.find(" ");
+        str3 = linha.substr(0,vazio);
+        std::cout << str3 << " ";
+        jogada_por_jogada.push_back(str3);
+        linha.erase(0,vazio+1);
+        if (str3.compare(ultimo)==0){
+            i=tamanho;
+            std::cout << std::endl;
+        }
+        
+        
+
+    }
+    /*
+    std::size_t vazio = jogadas[0].find(" ");
+    std::string str3 = jogadas[0].substr(0,vazio);
+    std::cout << str3 << " ";
+    linha.erase(0,vazio+1);
+
+    vazio = linha.find(" ");
+    str3 = linha.substr(0,vazio);
+    std::cout << str3 << " ";
+    linha.erase(0,vazio+1);
+    */
+    
+
+
+    for (int x =0; x<11; x++){
+        std::cout << linhas[x] << std::endl;
+    }
+    arq.close();
+    Pecas tabuleiro;
+    tabuleiro.imprime_tabuleiro();
+
+
     unsigned char vertical = 124;
     unsigned char horizontal = 196;
     unsigned char CantoSuperiorDireito = 35;
@@ -11,9 +76,8 @@ int main() {
     unsigned char IntercecaoEsquerda = 195;
     //std::cout << CantoSuperiorDireito << std::endl;
     
-    std::vector<char> tabuleiro[45][48];
 
-
+    /*
     for (int i=0; i<45 ; i++){
         if ((i>=1 && i<4) || (i>=5 && i<8) || (i>=9 && i<13)){
             for (int x=0; x<8; x++){
@@ -31,6 +95,7 @@ int main() {
             }   
         }                      
     }
-
+    */
+    
     return 0;
 }
